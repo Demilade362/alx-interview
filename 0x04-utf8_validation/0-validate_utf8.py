0c1ba68d5a238fc4950c94a83d943ef1e14a9cb1
Demilade362 <ademolademilade362@gmail.com>
2024-04-04 02:08:04 -0700

    utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Validate UTF8
+"""
+
+def validUTF8(data):
+    # Count of remaining continuation bytes required
+    remaining = 0
+
+    for byte in data:
+        # If no continuation bytes required
+        if remaining == 0:
+            # Single byte character
+            if byte >> 7 == 0:
+                continue
+            # Two-byte character
+            elif byte >> 5 == 0b110:
+                remaining = 1
+            # Three-byte character
+            elif byte >> 4 == 0b1110:
+                remaining = 2
+            # Four-byte character
+            elif byte >> 3 == 0b11110:
+                remaining = 3
+            else:
+                return False
+        else:
+            # Check if the byte starts with 10
+            if byte >> 6 != 0b10:
+                return False
+            remaining -= 1
+
+    # If there are remaining continuation bytes required
+    if remaining != 0:
+        return False
+
+    return True
